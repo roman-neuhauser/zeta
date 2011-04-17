4b468d523cbec40f1f124f1aa83d22a557e0d57c
Roman Neuhauser <neuhauser@sigpipe.cz>
2011-04-17 11:09:03 +0200

    tests: output uses builtin print or console.log
    intro.js throws if neither is available
    
    still a bit closer to CommonJS (node) compat

--- tests/intro.js
+++ tests/intro.js
@@ -3,5 +3,15 @@
 $$IMPORT_ZETA_INTO$$(this, { import_internals : true });
 
 var tests = [];
-var output = print;
+var output;
+if (typeof(print) != 'undefined') {
+  output = print;
+} else if (typeof(console) != 'undefined' && 'log' in console) {
+  output = function ()
+  {
+    return console.log.apply(console, arguments);
+  }
+} else {
+  throw "unsupported js shell.  please submit a patch.";
+};
 
